a72ffc68ef9625a7085b34431ccb890dbfbf10ff
竹笋 <polar.highness@gmail.com>
2023-06-06 19:42:43 +0800

    Update 标签爬取(未完成).py

--- 弱智操作/标签爬取(未完成).py
+++ 弱智操作/标签爬取(未完成).py
@@ -1,19 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-# 发起请求获取网页内容
 url = 'https://katzeimmondlicht.github.io/%E5%BC%B1%E6%99%BA%E6%93%8D%E4%BD%9C/tags/'  # 替换为目标网页的URL
 response = requests.get(url)
 html_content = response.text
 
-# 使用BeautifulSoup解析网页内容
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# 打开文件，指定文件名和打开模式（写入模式）
 file = open("C:\Users\meow\Documents\katzeimMondlicht.github.io\弱智操作\此时标签.txt", "w", encoding='utf-8')
 
-# 遍历 <p> 标签，将文本内容写入文件
-for p in paragraphs:
+for p in divmod:
     file.write(p.text)
     file.write("\n")  # 写入换行符，每个 <p> 标签的文本占一行
 
